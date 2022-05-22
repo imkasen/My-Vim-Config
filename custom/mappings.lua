e2512d6923698b75a32d1f1893640249e007ee61
Kasen <18170166+imkasen@users.noreply.github.com>
2022-05-22 17:05:20 +0800

    优化

--- custom/mappings.lua
+++ custom/mappings.lua
@@ -1,21 +1,9 @@
 local M = {}
 
-local function termcodes(str)
-    return vim.api.nvim_replace_termcodes(str, true, true, true)
-end
-
 M.general = {
-    i = {
-        ["jk"] = { "<ESC>", "   escape insert mode" },
-    },
-
     n = {
         ["<S-h>"] = { "^", "論 beginning of line" },
         ["<S-l>"] = { "$", "壟 end of line" },
-    },
-
-    t = {
-        ["jk"] = { termcodes "<C-\\><C-N>", "   escape terminal mode" },
     },
 }
 
