9ff9f6913b55aebda52d3ff37108b58776b21ec9
Kasen <18170166+imkasen@users.noreply.github.com>
2022-10-02 16:40:59 +0800

    plugins init: packer.nvim

---
+++ lua/plugins.lua
@@ -0,0 +1,58 @@
+-- 插件配置
+
+-- 自动安装 [packer.nvim](https://github.com/wbthomason/packer.nvim)
+-- ~/.local/share/nvim/site/pack/packer/
+local fn = vim.fn
+local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+local packer_bootstrap
+if fn.empty(fn.glob(install_path)) > 0 then
+    vim.notify("Installing packer.nvim, please wait...")
+    packer_bootstrap = fn.system({
+        "git",
+        "clone",
+        "--depth",
+        "1",
+        "https://github.com/wbthomason/packer.nvim",
+        install_path,
+    })
+    vim.notify("Installation is complete, please close and reopen NeoVim.")
+    vim.cmd [[packadd packer.nvim]] -- Only required if you have packer configured as `opt`
+end
+
+-- Use a protected call so we don't error out on first use
+local status_ok, packer = pcall(require, "packer")
+if not status_ok then
+    vim.notify("packer.nvim is not detected!")
+    return
+end
+
+-- Install your plugins here
+packer.startup({
+    function(use)
+        -- packer.nvim 可以升级自己
+        use("wbthomason/packer.nvim")
+
+        -- Automatically set up your configuration after cloning packer.nvim
+        -- Put this at the end after all plugins
+        if packer_bootstrap then
+            require("packer").sync()
+        end
+    end,
+
+    config = {
+        -- 自定义源
+        git = {
+            -- default_url_format = "https://github.com/%s",
+            default_url_format = "https://hub.fastgit.xyz/%s",
+        },
+        display = {
+            -- compact = false, -- If true, fold updates results by default
+            -- 使用浮动窗口显示
+            open_fn = function()
+                return require("packer.util").float({ border = "single" })
+            end,
+            -- show_all_info = true, -- Should packer show all update details automatically?
+            autoremove = true, -- Remove disabled or unused plugins without prompting the user
+        },
+    },
+})
